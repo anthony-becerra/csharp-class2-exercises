00842a2ff625c125ef860f57dc4c134f11457df2
Anthony Becerra <abece005@fiu.edu>
2017-09-14 13:57:14 -0400

    Add project files.

---
+++ Class2ExerciseList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class2ExerciseList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
